e4d380b8eae31acc28d4bcff92e5f2e7ef612333
felipeVoid <felipe.hernandez@rinno.la>
2019-10-13 14:07:36 -0300

    data center

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCXazFYniFR8eq5_lm7js8LXW2NQ485e90',
+    authDomain: 'a-club-admin.firebaseapp.com',
+    databaseURL: 'https://a-club-admin.firebaseio.com',
+    projectId: 'a-club-admin',
+    storageBucket: 'a-club-admin.appspot.com',
+    messagingSenderId: '110197724696'
+  }
 };
